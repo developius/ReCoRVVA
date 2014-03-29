26eaa05d57bfae946d442d7aff9a841a156641b3
musicboyben <benjames1999@hotmail.co.uk>
2014-03-29 19:17:59 +0000

    Update wiimote.py

--- recorvva.py
+++ recorvva.py
@@ -48,8 +48,7 @@ def commands():
 def connect():
 	try:
 		client_socket.connect(address)
-		client_socket.sendto("Client connected", address)
-		client_socket.sendto(" ", address)
+		send_msg("Client connected", address)
 		print colored("Connected to ReCoRVVA", 'blue')
 	except error:
 		print colored("Could not connect to ReCoRVVA", 'blue')
@@ -84,3 +83,10 @@ class data_thread (threading.Thread):
 				print colored("\n<SERVER> " + recv_data, 'red')
 			except error:
 				print colored("Could not get data", 'blue')
+
+connect()
+
+while True:
+	data = raw_input("<CONSOLE>: ")
+	print data
+	send_msg(data)
