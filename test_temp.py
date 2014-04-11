85333eb9422bd556765683788a3d53d7e6852552
musicboyben <benjames1999@hotmail.co.uk>
2014-04-11 07:46:59 +0100

    Added credits

--- test_temp.py
+++ test_temp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #---------------------------------------------------------------------------------------------------------+
-#						main.py							  |
-# Hamdles the multithreading and the other scripts.                                                       |
+#						test_temp.py				                  |
+# Takes a reading from the DHT-11 temperature sensor                                                      |
 # (c) 2014 F. Anderson (finnian@fxapi.co.uk) and B. James (benji@fxapi.co.uk)                             |
 #---------------------------------------------------------------------------------------------------------+
 
