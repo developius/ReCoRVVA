c2aa8721fff6558827dc972fb909902fc6258ff5
xavbabe <finnian@fxapi.co.uk>
2014-04-30 12:50:09 +0100

    Update xbox.py

--- client/xbox.py
+++ client/xbox.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python 
 #---------------------------------------------------------------------------------------------------------+
 #                                               xbox.py                                                   |
 # API and wrapper for communication with the RecoRVVA via a Python socket                                 |
