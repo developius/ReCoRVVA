d62ceeeefc1196f683212abe2e0f72f008b620a8
musicboyben <benjames1999@hotmail.co.uk>
2014-05-16 14:52:23 +0100

    Updated wiimote steering bug

--- client/wiimote.py
+++ client/wiimote.py
@@ -46,6 +46,8 @@ print("Press PLUS and MINUS together to disconnect and quit.\n")
 #turn on reporting mode
 wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
 
+moving = False
+
 tilt_status = 'right'#accelerometer reading is (0,0,0) first time through the while true.
 
 while True:
@@ -53,26 +55,27 @@ while True:
   buttons = wm.state['buttons']
   accelerometer = wm.state['acc']
 
-  #check whether the Wiimote is tilted to the right or left
-  #accelerometer reading increases as Wiimote tilts left and decreases as it's tilting right. Neutral is 125.
-  #print acceleromter
-  if(accelerometer[1] > 130):
-    if(tilt_status != 'left'):
-       print("turning left")
-       recorvva.send_msg("L")
-       tilt_status = 'left'
-
-  elif(accelerometer[1] < 110):
-    if(tilt_status != 'right'):
-      print("turning right")
-      recorvva.send_msg("R")
-      tilt_status = 'right'
-
-  elif(accelerometer[1] <= 130 & accelerometer[1] >= 110):
-    if(tilt_status != 'neutral'):
-      print("steering straight")
-      recorvva.send_msg("N")
-      tilt_status = 'neutral'
+  if(moving == True):
+    #check whether the Wiimote is tilted to the right or left
+    #accelerometer reading increases as Wiimote tilts left and decreases as it's tilting right. Neutral is 125.
+    #print acceleromter
+    if(accelerometer[1] > 130):
+      if(tilt_status != 'left'):
+         print("turning left")
+         recorvva.send_msg("L")
+         tilt_status = 'left'
+  
+    elif(accelerometer[1] < 110):
+      if(tilt_status != 'right'):
+        print("turning right")
+        recorvva.send_msg("R")
+        tilt_status = 'right'
+  
+    elif(accelerometer[1] <= 130 & accelerometer[1] >= 110):
+      if(tilt_status != 'neutral'):
+        print("steering straight")
+        recorvva.send_msg("N")
+        tilt_status = 'neutral'
 
   # If Plus and Minus buttons pressed together then rumble and quit.
   if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
@@ -106,11 +109,13 @@ while True:
   if (buttons & cwiid.BTN_1):
     print("Button 1 pressed")
     recorvva.send_msg("B")
+    moving = True
     time.sleep(button_delay)
 
   if (buttons & cwiid.BTN_2):
     print("Going forwards")
     recorvva.send_msg("F") 
+    moving = True
     time.sleep(button_delay)
     
   if (buttons & cwiid.BTN_A):
@@ -129,4 +134,5 @@ while True:
   if (buttons & cwiid.BTN_B):
     print("Stopping")
     recorvva.send_msg("S")
+    moving = False
     time.sleep(button_delay)
