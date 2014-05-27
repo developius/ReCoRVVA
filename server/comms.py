bfe6cefbaaa2f11f5e671ccfcf8d6a44152c5ea1
Finnian Anderson <finnian@fxapi.co.uk>
2014-05-27 19:25:23 +0100

    Bug fix, now allows new connection when last connection finishes

--- server/comms.py
+++ server/comms.py
@@ -11,16 +11,16 @@ from termcolor import colored
 
 # define our address ('' means all available addresses) and port (7777)
 address = ('', 7777)
-# our socket is a stream and we can reuse it
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
 # client_socket var is empty
 client_socket = None
 
 # available commands
 CMDS = ["A","L","D","R","W","F","X","B","S","Stop","CamOn","CamOff","pan_left","pan_right","pan_center","tilt_forwards","tilt_backwards","tilt_up","HH","HL"]
 # what it says on the tin
-authorised_addresses = ["fxap.home", "fxap.local", "pimin.home", "pimin.local", "25.110.219.165", "Xav'sPad", "snail.local"]
+authorised_addresses = ["fxapi.home", "fxapi.local", "pimine.home", "pimine.local", "25.110.219.165", "Xav'sPad", "snail.local"]
 
 def sendToUI(msg): # function for sending to client's socket
 	if client_socket is not None:
@@ -38,14 +38,15 @@ def test_conn(): # function for testing the connection
 
 class Comms (threading.Thread):
         def run (self):
+		global server_socket
 		try: # to bind the address
 			server_socket.bind(address)
 		except Exception, e:
-		        print colored("Address already in use", 'red')
-			server_socket.shutdown()
+		        print colored("Address already in use: " + str(e), 'red')
+			server_socket.shutdown(1)
 			server_socket.close()
-		        sys.exit()
-
+			os.system('sudo killall -9 python')
+			
 		server_socket.listen(2) # listen for max 2 clients
 
 		print colored("Socket ready", 'blue')
@@ -66,39 +67,31 @@ class Comms (threading.Thread):
 				print colored("Unauthorised connection attempted - " + str(host) + " - closing their socket", 'red')
 				client_socket.send("You are not authorised to connect - sorry. Go die in a hole.")
 				client_socket.close() # close their socket
-				print colored("Socket closed to client", 'red')
 				client_socket = None
-
-			if client_socket.recv() == 0:
-					client_socket.close()
-					server_socket = None	
 
 			if client_socket is not None: # if they ARE authorised...
 				client_socket.send("Welcome") # ...welcome them!
 
-			if client_socket is None: #or test_conn == False:
-                                print "Client %s disconnected" % host
-                                break
+				while True:
+					if client_socket is not None:
+	        	                        recv_data = client_socket.recv(2048) # get data
+						if not recv_data or client_socket.recv() == 0:
+							print colored("Client %s disconnected" % host, 'red')
+							client_socket.close()
+			                                break
 
+						if (recv_data in CMDS) == True: # if it's a command
+							motors.move(recv_data) # tell the motors what it is
+							cam.camera(recv_data) # tell the camera what it is
+							cam.servo(recv_data)
 
-			while True:
-				if client_socket is not None:
-#					try:
-        	                        recv_data = client_socket.recv(2048) # get data
-					if not recv_data:
-						print "Client %s disconnected" % host
-						client_socket.close()
-		                                break
-#	        	                except:
-#	                	                recv_data = ""
-					if (recv_data in CMDS) == True: # if it's a command
-						motors.move(recv_data) # tell the motors what it is
-						cam.camera(recv_data) # tell the camera what it is
-						cam.servo(recv_data)
-					if recv_data == "":
-						pass
-					if recv_data == " ":
-						pass
-					if recv_data not in CMDS: # if it's not any of the above, it's something else and we may need to know what
-#						print colored("Received: '" + recv_data + "' from '" + str(host) + "'", 'blue')
-						pass
+						if recv_data == "":
+							pass
+						if recv_data == " ":
+							pass
+
+						if recv_data not in CMDS: # if it's not any of the above, it's something else and we may need to know what
+#							print colored("Received: '" + recv_data + "' from '" + str(host) + "'", 'blue')
+							pass
+			elif client_socket is None: #or test_conn == False:
+                                print colored("Client %s disconnected" % host, 'red')
